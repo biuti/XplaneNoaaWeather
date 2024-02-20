cd98fc7de38e126e3b7eba045c5db187f5a805ea
Antonio Golfari <anyoniogolfari@gmail.com>
2024-02-20 14:30:17 +0100

    bug fixed

--- noaaweather/weatherServer.py
+++ noaaweather/weatherServer.py
@@ -134,7 +134,7 @@ class ClientHandler(SocketServer.BaseRequestHandler):
 
     def handle(self):
         response = False
-        data = self.request[0].decode('utf-8').strip("\n\c\t ")
+        data = self.request[0].decode('utf-8').strip("\n\r\t")
 
         if len(data) > 1:
             if data[0] == '?':
@@ -212,7 +212,6 @@ if __name__ == "__main__":
 
     if not debug:
         logfile = LogFile(Path(conf.respath, 'weatherServerLog.txt'), 'w')
-
         sys.stderr = logfile
         sys.stdout = logfile
 
