7444dd3cebb5ccb0146bbb9482e9cf36f0236dce
Antonio Golfari <anyoniogolfari@gmail.com>
2025-11-13 19:11:58 +0100

    version bump

--- noaaweather/conf.py
+++ noaaweather/conf.py
@@ -25,6 +25,7 @@ class Conf:
     syspath, dirsep = '', os.sep
     printableChars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~ '
 
+    __VERSION__ = '12.3-rc.1'
 
     # Dref and Command parameters
     plugin_command_origin = 'XPNOAAWeather'
